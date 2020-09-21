ba6e78af3b4f0beb242714360fd2bf738a0d9cf9
Omris-Elda <ed.prickett@hotmail.com>
2020-09-21 13:42:59 +0100

    Added public ips

--- VPC/main.tf
+++ VPC/main.tf
@@ -7,18 +7,22 @@ data "aws_availability_zones" "available" {
 }
 
 resource "aws_subnet" "public_subnet-1" {
-  cidr_block        = var.public-subnet-cidr-block-1
-  availability_zone = data.aws_availability_zones.available.names[0]
-  vpc_id            = aws_vpc.FP_VPC.id
+  cidr_block              = var.public-subnet-cidr-block-1
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  vpc_id                  = aws_vpc.FP_VPC.id
+  map_public_ip_on_launch = true
+
   tags = {
     "kubernetes.io/cluster/basic-cluster" = "shared"
   }
 }
 
 resource "aws_subnet" "public_subnet-2" {
-  cidr_block        = var.public-subnet-cidr-block-2
-  availability_zone = data.aws_availability_zones.available.names[1]
-  vpc_id            = aws_vpc.FP_VPC.id
+  cidr_block              = var.public-subnet-cidr-block-2
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  vpc_id                  = aws_vpc.FP_VPC.id
+  map_public_ip_on_launch = true
+
   tags = {
     "kubernetes.io/cluster/basic-cluster" = "shared"
   }
